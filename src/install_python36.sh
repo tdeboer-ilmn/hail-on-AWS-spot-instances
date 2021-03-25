cdeafa9646e64dd73415c36929ce5c2005f125b7
tdeboer-ilmn <tdeboer@illumina.com>
2021-03-25 00:31:04 +0000

    Fixed install_python36.py

--- src/install_python36.sh
+++ src/install_python36.sh
@@ -5,45 +5,49 @@ export PATH=$PATH:/usr/local/bin
 sudo yum install python36 python36-devel python36-setuptools -y 
 sudo easy_install pip
 sudo python3 -m pip install --upgrade pip
+sudo python3 -m pip install 'setuptools>=45'
 
 if grep isMaster /mnt/var/lib/info/instance.json | grep true; then
-	# Master node: Install all
-	WHEELS="pyserial
-	oauth
-	argparse
-	parsimonious
-	wheel
-	pandas
-	utils
-	ipywidgets
-	numpy
-	scipy
-	bokeh
-	requests
-	boto3
-	selenium
-	pillow
-	jupyterlab"
+    sudo yum install g++ cmake git -y
+    sudo yum install gcc72-c++ -y # Fixes issue with c++14 incompatibility in Amazon Linux
+    sudo yum install lz4 lz4-devel -y # Fixes issue of missing lz4
+    # Master node: Install all
+    WHEELS="pyserial
+    oauth
+    argparse
+    parsimonious
+    wheel
+    pandas
+    utils
+    ipywidgets
+    numpy
+    scipy
+    bokeh
+    requests
+    boto3
+    selenium
+    pillow
+    jupyterlab"
 else 
-	# Worker node: Install all but jupyter lab
-	WHEELS="pyserial
-	oauth
-	argparse
-	parsimonious
-	wheel
-	pandas
-	utils
-	ipywidgets
-	numpy
-	scipy
-	bokeh
-	requests
-	boto3
-	selenium
-	pillow"
+    # Worker node: Install all but jupyter lab
+    WHEELS="pyserial
+    oauth
+    argparse
+    parsimonious
+    wheel
+    pandas
+    utils
+    ipywidgets
+    numpy
+    scipy
+    bokeh
+    requests
+    boto3
+    selenium
+    pillow"
 fi
 
 for WHEEL_NAME in $WHEELS
 do
-	sudo python3 -m pip install $WHEEL_NAME
+    sudo python3 -m pip install $WHEEL_NAME
 done
