a4b55b2625f1810e89a2f99d35fdb10354a1f6cc
tdeboer-ilmn <tdeboer@illumina.com>
2021-03-25 22:55:19 +0000

    Jupyter password was not working still

--- src/jupyter_notebook_config.py
+++ src/jupyter_notebook_config.py
@@ -2,6 +2,6 @@ c.NotebookApp.open_browser = False
 c.NotebookApp.ip='0.0.0.0' #'*'
 c.NotebookApp.port = 8192 # If you change the port here, make sure you update it in the jupyter_installer.sh file as well
 #Is now: tdeboer-ilmn
-c.NotebookApp.password = u'sha1:6ccbd197847ec7a0780ea689a2161fbb620ecbdb'
+c.NotebookApp.password = u'sha1:fd9cac42d6b4:aa89914827e5fabf77633ce74686ffd409ceb6b5'
 c.Authenticator.admin_users = {'jupyter'}
 c.LocalAuthenticator.create_system_users = True
