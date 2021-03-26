16e72f0f7319b4a4011e62ddca227d3981e82a3c
tdeboer-ilmn <tdeboer@illumina.com>
2021-03-26 04:32:01 +0000

    Removed the SPARK_HOME references to make the notebooks work

--- src/jupyter_run.sh
+++ src/jupyter_run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-export SPARK_HOME=/usr/lib/spark
+#export SPARK_HOME=/usr/lib/spark
 export PYSPARK_PYTHON=python3
 export HAIL_HOME=/opt/hail-on-AWS-spot-instances
 
-export PYTHONPATH="/home/hadoop/hail-python.zip:$SPARK_HOME/python:${SPARK_HOME}/python/lib/py4j-src.zip"
-echo "PYTHONPATH: ${PYTHONPATH}"
+# export PYTHONPATH="/home/hadoop/hail-python.zip:$SPARK_HOME/python:${SPARK_HOME}/python/lib/py4j-src.zip"
+# echo "PYTHONPATH: ${PYTHONPATH}"
 
 export PYSPARK_PYTHON=python3
 echo "PYSPARK_PYTHON: ${PYSPARK_PYTHON}"
 
 # Needed for HDFS
-JAR_PATH="/home/hadoop/hail-all-spark.jar:/usr/share/aws/emr/emrfs/lib/emrfs-hadoop-assembly-2.32.0.jar"
+JAR_PATH="/usr/share/aws/emr/emrfs/lib/emrfs-hadoop-assembly-2.32.0.jar"
 export PYSPARK_SUBMIT_ARGS="--conf spark.driver.extraClassPath='$JAR_PATH' --conf spark.executor.extraClassPath='$JAR_PATH' pyspark-shell"
 echo "PYSPARK_SUBMIT_ARGS: ${PYSPARK_SUBMIT_ARGS}"
 
